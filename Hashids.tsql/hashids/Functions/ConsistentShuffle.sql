5bb3d2768a8b921c40ef56e56e7cbdbec7818ea8
Wayne Bloss <waynebloss@gmail.com>
2014-11-06 12:04:29 -0500

    Added SQL Server project created with VS2013 Pro, targeting SQL Server 2008. Includes [hashids] schema and some basic functions ported from the reference projects.

---
+++ Hashids.tsql/hashids/Functions/ConsistentShuffle.sql
@@ -0,0 +1,49 @@
+﻿CREATE FUNCTION [hashids].[ConsistentShuffle]
+(
+	@alphabet varchar(255),
+	@salt varchar(255)
+)
+RETURNS varchar(255)
+AS
+BEGIN
+	
+	IF [hashids].[IsNullOrWhitespace](@salt) = 1 BEGIN
+		RETURN @alphabet;
+	END
+
+	DECLARE
+		@ls int = LEN(@salt),
+		@i int = LEN(@alphabet) - 1,
+		@v int = 0,
+		@p int = 0, 
+		@n int = 0,
+		@j int = 0,
+		@temp varchar(1);
+
+	WHILE @i > 0 BEGIN
+		
+		SELECT
+			@v = @v % @ls,
+			@n = ASCII(SUBSTRING(@salt, @v + 1, 1)),
+			@p = @p + @n,
+			@j = (@n + @v + @p) % @i,
+			@temp = SUBSTRING(@alphabet, @j + 1, 1),
+
+			@alphabet = 
+				SUBSTRING(@alphabet, 1, @j) + 
+				SUBSTRING(@alphabet, @i + 1, 1) + 
+				SUBSTRING(@alphabet, @j + 2, 255),
+
+			@alphabet = 
+				SUBSTRING(@alphabet, 1, @i) + 
+				@temp + 
+				SUBSTRING(@alphabet, @i + 2, 255),
+
+			@i = @i - 1,
+			@v = @v + 1;
+
+	END -- WHILE
+
+	RETURN @alphabet;
+
+END
---
+++ Hashids.tsql/hashids/Functions/IsNullOrWhitespace.sql
@@ -0,0 +1,15 @@
+﻿CREATE FUNCTION [hashids].[IsNullOrWhitespace]
+(
+	@value varchar(255)
+)
+RETURNS bit
+AS
+BEGIN
+	
+	IF @value IS NULL RETURN 1;
+
+	IF LEN(LTRIM(RTRIM(@value))) = 0 RETURN 1;
+
+	RETURN 0;
+
+END
