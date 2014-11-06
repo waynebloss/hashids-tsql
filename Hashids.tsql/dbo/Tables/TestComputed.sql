683363cdbe16730cce9424300789a45121ebb322
Wayne Bloss <waynebloss@gmail.com>
2014-11-06 17:57:45 -0500

    Added table dbo.TestComputed.

---
+++ Hashids.tsql/dbo/Tables/TestComputed.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[TestComputed] (
+    [Id]     INT           IDENTITY (1, 1) NOT NULL,
+    [hashId] AS            ([hashids].[EncodeId]([Id]) collate sql_latin1_general_cp1_cs_as) PERSISTED,
+    [Name]   VARCHAR (255) NULL,
+    CONSTRAINT [PK_TestComputed] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
