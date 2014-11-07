9df6e02c32d5d3e97624b6e3a48a7dc3f55686f2
Wayne Bloss <waynebloss@gmail.com>
2014-11-06 19:41:12 -0500

    Renamed table.

---
+++ db/mssql/dbo/Tables/ComputedTest.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ComputedTest] (
+    [Id]     INT           IDENTITY (1, 1) NOT NULL,
+    [HashId] AS            ([hashids].[encodeId]([Id]) collate sql_latin1_general_cp1_cs_as) PERSISTED,
+    [Name]   VARCHAR (255) NULL,
+    CONSTRAINT [PK_ComputedTest] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+
