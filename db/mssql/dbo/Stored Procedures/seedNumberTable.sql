2053c527e38205e9d6f59b4041fae6e4ed515817
Wayne Bloss <waynebloss@gmail.com>
2014-11-06 19:33:10 -0500

    Added Number table and seedNumberTable procedure.

---
+++ db/mssql/dbo/Stored Procedures/seedNumberTable.sql
@@ -0,0 +1,47 @@
+﻿CREATE PROCEDURE [dbo].[seedNumberTable]
+	@rows int = NULL,
+	@withStringConversion bit = NULL
+AS
+BEGIN
+	
+	SELECT
+		@rows = IsNull(@rows, 8192),
+		@withStringConversion = IsNull(@withStringConversion, 1);
+
+	TRUNCATE TABLE [dbo].[Number];
+	
+	-- Create 8 seed rows
+	DECLARE
+		@seed TABLE(i int);
+
+	INSERT INTO @seed(i)
+	VALUES(0),(1),(2),(3),(4),(5),(6),(7);
+
+	INSERT INTO [dbo].[Number](i)
+	SELECT i FROM @seed WHERE i < @rows;
+
+	-- Multiply them by 2 until we reach @rows
+	DECLARE
+		@count int
+
+	WHILE ((select count(*) from [dbo].[Number]) < @rows) BEGIN
+		
+		SELECT @count = (select count(*) from [dbo].[Number])
+
+		INSERT INTO [dbo].[Number](i)
+		SELECT n.i + @count
+		FROM [dbo].[Number] n
+		WHERE
+			(n.i + @count) < @rows;
+
+	END
+
+	-- Pre-generate string conversions
+	IF @withStringConversion = 1 BEGIN
+		UPDATE [dbo].[Number]
+		SET
+			ia = cast(i as varchar(10)),
+			iu = cast(i as nvarchar(10));
+	END
+
+END
---
+++ db/mssql/dbo/Tables/Number.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Number] (
+    [i]  INT           NOT NULL,
+    [ia] VARCHAR (10)  NULL,
+    [iu] NVARCHAR (10) NULL,
+    CONSTRAINT [PK_Integers] PRIMARY KEY CLUSTERED ([i] ASC)
+);
