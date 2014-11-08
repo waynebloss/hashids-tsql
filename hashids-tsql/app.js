3ad96c5e5206901f21bfa7664f3e56691b430693
Wayne Bloss <waynebloss@gmail.com>
2014-11-08 00:21:36 -0500

    Added option to output to a directory of separate files.

--- hashids-tsql/app.js
+++ hashids-tsql/app.js
@@ -1,5 +1,8 @@
-﻿var pkg = require('./package.json');
+﻿'use strict';
+
+var pkg = require('./package.json');
 var fs = require('fs');
+var path = require('path');
 var Hashids = require('hashids');
 var swig = require('swig');
 var app = require('commander');
@@ -7,7 +10,9 @@ var defv = {
   salt: makeSalt(),
   minHashLength: 0,
   alphabet: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890',
-  seps: 'cfhistuCFHISTU'
+  seps: 'cfhistuCFHISTU',
+  fileExt: 'sql',
+  schema: 'hashids'
 };
 
 //Array.prototype.push.call(process.argv, '-d');
@@ -15,13 +20,14 @@ var defv = {
 
 app
   .version(pkg.version)
-  .usage('[options] [file]')
+  .usage('[options] [file or directory/ path]')
   .option('-d, --database [name]', 'Database name.')
   .option('-n, --schema [name]', 'Database schema [hashids].', 'hashids')
   .option('-s, --salt [value]', 'Salt. [random]', defv.salt)
   .option('-l, --minHashLength [n]', 'Minimum hash length. [' + defv.minHashLength.toString() + ']', defv.minHashLength)
   .option('-a, --alphabet [value]', 'Alphabet. [a-z,A-Z,1-9,0]', defv.alphabet)
   .option('-e, --encodeOnly', 'Script encode function(s) only.')
+  .option('-x, --fileExt [value]', 'Extension for output files. [' + defv.fileExt + ']', defv.fileExt)
   .parse(process.argv);
 
 run();
@@ -29,10 +35,16 @@ run();
 function run() {
   var data = getHashData();
   var tplFiles = getTemplates();
-  var i, output, filename;
+  var i, output, file;
   for (i = 0; i < tplFiles.length; i++) {
+    if (data.directoryName) {
+      file = path.basename(tplFiles[i], '.swig');
+      if (file === 'db')
+        continue;
+      data.fileName = path.join(data.directoryName, file + '.' + app.fileExt);
+    }
     output = swig.renderFile(tplFiles[i], data);
-    pipeWriteOrAppend(output, data, i === 0);
+    pipeWriteOrAppend(output, data, (i === 0) || data.directoryName);
   }
 }
 /**
@@ -50,12 +62,29 @@ function getHashData() {
     guards: hashids.guards,
     minHashLength: hashids.minHashLength,
     pipe: app.args.length === 0,
-    fileName: ''
+    fileName: '',
+    directoryName: ''
   };
-  if (!data.pipe)
+  if (!data.pipe) {
     data.fileName = app.args[0];
+    data.directoryName = getDirectoryName(data);
+  }
   return data;
 }
+/**
+ * Returns the directory name that should be applied to the given data.
+ */
+function getDirectoryName(data) {
+  var len = data.fileName.length,
+      last;
+  if (len === 0)
+    return '';
+  last = data.fileName[len - 1];
+  if (last === '/' || last === '\\')
+    return data.fileName;
+  return '';
+}
+
 /**
  * Get the templates that should be rendered based on app options.
  */
