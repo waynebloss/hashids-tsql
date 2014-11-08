59399c6f0e4cbc505e216d289d687b3dcc0d41bc
Wayne Bloss <waynebloss@gmail.com>
2014-11-07 19:00:13 -0500

    Use SET instead of SELECT.

--- db/mssql/hashids/Functions/consistentShuffle.sql
+++ db/mssql/hashids/Functions/consistentShuffle.sql
@@ -20,29 +20,25 @@ BEGIN
 		@p int = 0, 
 		@n int = 0,
 		@j int = 0,
-		@temp varchar(1);
+		@temp char(1);
 
 	WHILE @i > 0 BEGIN
 		
-		SELECT
-			@v = @v % @ls,
-			@n = ASCII(SUBSTRING(@salt, @v + 1, 1)),
-			@p = @p + @n,
-			@j = (@n + @v + @p) % @i,
-			@temp = SUBSTRING(@alphabet, @j + 1, 1),
-
-			@alphabet = 
+		SET @v = @v % @ls;
+		SET @n = ASCII(SUBSTRING(@salt, @v + 1, 1));
+		SET @p = @p + @n;
+		SET @j = (@n + @v + @p) % @i;
+		SET @temp = SUBSTRING(@alphabet, @j + 1, 1);
+		SET @alphabet = 
 				SUBSTRING(@alphabet, 1, @j) + 
 				SUBSTRING(@alphabet, @i + 1, 1) + 
-				SUBSTRING(@alphabet, @j + 2, 255),
-
-			@alphabet = 
+				SUBSTRING(@alphabet, @j + 2, 255);
+		SET @alphabet = 
 				SUBSTRING(@alphabet, 1, @i) + 
 				@temp + 
-				SUBSTRING(@alphabet, @i + 2, 255),
-
-			@i = @i - 1,
-			@v = @v + 1;
+				SUBSTRING(@alphabet, @i + 2, 255);
+		SET @i = @i - 1;
+		SET @v = @v + 1;
 
 	END -- WHILE
 
--- db/mssql/hashids/Functions/hash.sql
+++ db/mssql/hashids/Functions/hash.sql
@@ -10,15 +10,15 @@ BEGIN
 	DECLARE
 		@hash varchar(255) = '',
 		@alphabetLength int = LEN(@alphabet),
-		@pos int = 0;
+		@pos int;
 
 	WHILE 1 = 1 BEGIN
-		SELECT
-			@pos = @input % @alphabetLength,
-			@hash = SUBSTRING(@alphabet, @pos + 1, 1) + @hash,
-			@input = CAST((@input / @alphabetLength) as int);
-		IF @input <= 0 BREAK;
+		SET @pos = @input % @alphabetLength;
+		SET @hash = SUBSTRING(@alphabet, @pos + 1, 1) + @hash;
+		SET @input = CAST((@input / @alphabetLength) as int);
+		IF @input <= 0
+			BREAK;
 	END
-	
+
 	RETURN @hash;
 END
