4beb2d598cd0bab1d7e59669076bbb0b20c46914
Wayne Bloss <waynebloss@gmail.com>
2014-11-07 23:18:41 -0500

    Now outputting all swig templates to stdout or a file.

--- hashids-tsql/app.js
+++ hashids-tsql/app.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var Hashids = require('hashids');
 var swig = require('swig');
 var app = require('commander');
-var def = {
+var defv = {
   salt: makeSalt(),
   minHashLength: 0,
   alphabet: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890',
@@ -13,32 +13,69 @@ var def = {
 app
   .version(pkg.version)
   .usage('[options] [file]')
-  .option('-s, --salt [value]', 'Salt. [random]', def.salt)
-  .option('-m, --minHashLength [n]', 'Minimum hash length. [' + def.minHashLength.toString() + ']', def.minHashLength)
-  .option('-a, --alphabet [value]', 'Alphabet. [a-z,A-Z,1-9,0]', def.alphabet)
+  .option('-s, --salt          [value]', 'Salt. [random]', defv.salt)
+  .option('-m, --minHashLength [n]', 'Minimum hash length. [' + defv.minHashLength.toString() + ']', defv.minHashLength)
+  .option('-a, --alphabet      [value]', 'Alphabet. [a-z,A-Z,1-9,0]', defv.alphabet)
+  .option('-e, --encodeOnly', 'Script encode function(s) only.')
   .parse(process.argv);
-
-if (app.args.length === 0)
-  app.args.push('./hashids.encodeId.sql');
 
 run();
 
 function run() {
+  var data = getHashData();
+  var tplFiles = getTemplates();
+  var i, output;
+  for (i = 0; i < tplFiles.length; i++) {
+    output = swig.renderFile(tplFiles[i], data);
+    pipeWriteOrAppend(output, data, i === 0);
+  }
+}
+/**
+ * Gets the working data from a hashids.js instance, based on app options.
+ */
+function getHashData() {
   var hashids = new Hashids(app.salt, app.minHashLength, app.alphabet);
   var data = {
+    startingAlphabet: app.alphabet,
     salt: hashids.salt,
     alphabet: hashids.alphabet,
     seps: hashids.seps,
     guards: hashids.guards,
     minHashLength: hashids.minHashLength,
-    fileName: app.args[0]
+    pipe: app.args.length === 0,
+    fileName: ''
   };
-  var output = swig.renderFile('./templates/tsql/encodeId.swig', data);
-  fs.writeFileSync(data.fileName, output);
+  if (!data.pipe)
+    data.fileName = app.args[0];
+  return data;
 }
-
 /**
- * Creates a new salt value.
+ * Get the templates that should be rendered based on app options.
+ */
+function getTemplates() {
+  if (app.encodeOnly)
+    return [
+      './templates/tsql/encodeId.swig'
+    ];
+  return [
+    './templates/tsql/consistentShuffle.swig',
+    './templates/tsql/hash.swig',
+    './templates/tsql/encodeId.swig'
+  ];
+}
+/**
+ * Pipes to stdout or writes (or appends) to a file based on the given options.
+ */
+function pipeWriteOrAppend(output, opt, isFirstOutput) {
+  if (opt.pipe)
+    console.log(output);  // CONSIDER: Is this the best way to pipe to stdout?
+  else if (isFirstOutput)
+    fs.writeFileSync(opt.fileName, output);
+  else
+    fs.appendFileSync(opt.fileName, output);
+}
+/**
+ * Creates a new random salt value.
  */
 function makeSalt() {
   var hrtime = process.hrtime(),
