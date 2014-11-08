e7fd9456a76dbcff3a7b76955fbc010bdc0e17f2
Wayne Bloss <waynebloss@gmail.com>
2014-11-08 13:51:11 -0500

    Added some *A and *B functions.

---
+++ db/mssql/hashids/Functions/hashA.sql
@@ -0,0 +1,24 @@
+﻿CREATE FUNCTION [hashids].[hashA]
+(
+	@input int,
+	@alphabet varchar(255)
+)
+RETURNS varchar(255)
+WITH SCHEMABINDING
+AS
+BEGIN
+	DECLARE
+		@hash varchar(255) = '',
+		@alphabetLength int = LEN(@alphabet),
+		@pos int;
+
+	WHILE 1 = 1 BEGIN
+		SET @pos = @input % @alphabetLength;
+		SET @hash = SUBSTRING(@alphabet, @pos + 1, 1) + @hash;
+		SET @input = CAST((@input / @alphabetLength) as int);
+		IF @input <= 0
+			BREAK;
+	END
+
+	RETURN @hash;
+END
