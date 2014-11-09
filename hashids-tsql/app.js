8c40a4d0914f3fe70e14e1aa263929028e037ab0
Wayne Bloss <waynebloss@gmail.com>
2014-11-09 14:07:51 -0500

    Working on generator.

--- hashids-tsql/app.js
+++ hashids-tsql/app.js
@@ -5,21 +5,24 @@ var fs = require('fs');
 var path = require('path');
 var Hashids = require('hashids');
 var swig = require('swig');
+var mkdirp = require('mkdirp');
+
 var app = require('commander');
 var defv = {
+  database: 'HashidsTsql',
+  schema: 'hashids',
   salt: makeSalt(),
   minHashLength: 0,
   alphabet: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890',
   seps: 'cfhistuCFHISTU',
-  fileExt: 'sql',
-  schema: 'hashids'
+  fileExt: 'sql'
 };
 
 app
   .version(pkg.version)
   .usage('[options] [file path or directory/ path]')
-  .option('-d, --database [name]', 'Database name.')
-  .option('-n, --schema [name]', 'Database schema [hashids].', 'hashids')
+  .option('-d, --database [name]', 'Database name.', defv.database)
+  .option('-n, --schema [name]', 'Database schema [hashids].', defv.schema)
   .option('-s, --salt [value]', 'Salt. [random]', defv.salt)
   .option('-l, --minHashLength [n]', 'Minimum hash length. [' + defv.minHashLength.toString() + ']', defv.minHashLength)
   .option('-a, --alphabet [value]', 'Alphabet. [a-z,A-Z,1-9,0]', defv.alphabet)
@@ -27,28 +30,53 @@ app
   .option('-x, --fileExt [value]', 'Extension for output files. [' + defv.fileExt + ']', defv.fileExt)
   .parse(process.argv);
 
-// TODO: Error if the destination is a directory (ends in slash) and it doesn't exist.
-
 run();
 
 function run() {
   var data = getHashData();
-  var tplFiles = getTemplates();
+  var tpl = getTemplates();
+  
+  if (data.directoryName)
+    renderToDirectory(data, tpl);
+  else if (data.fileName)
+    renderToFile(data, tpl);
+  else
+    renderToStdout(data, tpl);
+}
+
+function renderToDirectory(data, tpl) {
   var i, output, file;
-  // TODO: Break this loop out into 2 separate functions, 
-  // one for outputting to a directory and the other for a single file.
-  // TODO: When outputting to directory, prefix each file with db.swig.
-  for (i = 0; i < tplFiles.length; i++) {
-    if (data.directoryName) {
-      file = path.basename(tplFiles[i], '.swig');
-      if (file === 'db')
-        continue;
-      data.fileName = path.join(data.directoryName, file + '.' + app.fileExt);
-    }
-    output = swig.renderFile(tplFiles[i], data);
-    pipeWriteOrAppend(output, data, (i === 0) || data.directoryName);
+  mkdirp.sync(data.directoryName);
+  for (i = 0; i < tpl.files.length; i++) {
+    file = path.basename(tpl.files[i], '.swig');
+    if (file === 'db')
+      continue;
+    data.fileName = path.join(data.directoryName, file + '.' + app.fileExt);
+    output = swig.renderFile(tpl.files[i], data);
+    output = tpl.db(data) + output;
+    fs.writeFileSync(data.fileName, output);
   }
 }
+
+function renderToFile(data, tpl) {
+  var i, output;
+  for (i = 0; i < tpl.files.length; i++) {
+    output = swig.renderFile(tpl.files[i], data);
+    if (i === 0)
+      fs.writeFileSync(data.fileName, output);
+    else
+      fs.appendFileSync(data.fileName, output);
+  }
+}
+
+function renderToStdout(data, tpl) {
+  var i, output;
+  for (i = 0; i < tpl.files.length; i++) {
+    output = swig.renderFile(tpl.files[i], data);
+    console.log(output);  // CONSIDER: Is this the best way to pipe to stdout?
+  }
+}
+
 /**
  * Gets the working data from a hashids.js instance, based on app options.
  */
@@ -91,29 +119,27 @@ function getDirectoryName(data) {
  * Get the templates that should be rendered based on app options.
  */
 function getTemplates() {
-  var tpls = app.encodeOnly ? [
-    './templates/tsql/Functions/encode1.swig',
-    './templates/tsql/Functions/encode2.swig'
-  ] : [
-    './templates/tsql/Functions/consistentShuffle.swig',
-    './templates/tsql/Functions/hash.swig',
-    './templates/tsql/Functions/encode1.swig',
-    './templates/tsql/Functions/encode2.swig'
-  ];
-  if (app.database)
-    tpls.unshift('./templates/tsql/db.swig');
-  return tpls;
-}
-/**
- * Pipes to stdout or writes (or appends) to a file based on the given options.
- */
-function pipeWriteOrAppend(output, opt, isFirstOutput) {
-  if (opt.pipe)
-    console.log(output);  // CONSIDER: Is this the best way to pipe to stdout?
-  else if (isFirstOutput)
-    fs.writeFileSync(opt.fileName, output);
-  else
-    fs.appendFileSync(opt.fileName, output);
+  var tpl = {
+    files: app.encodeOnly ? [
+      './templates/tsql/Functions/encode1.swig',
+      './templates/tsql/Functions/encode2.swig',
+      './templates/tsql/Functions/encodeList.swig',
+      './templates/tsql/Functions/encodeSplit.swig'
+    ] : [
+      './templates/tsql/schema.swig',
+      './templates/tsql/User Defined Types/ListOfBigint.swig',
+      './templates/tsql/User Defined Types/ListOfInt.swig',
+      './templates/tsql/Functions/consistentShuffle.swig',
+      './templates/tsql/Functions/hash.swig',
+      './templates/tsql/Functions/encode1.swig',
+      './templates/tsql/Functions/encode2.swig',
+      './templates/tsql/Functions/encodeList.swig',
+      './templates/tsql/Functions/encodeSplit.swig'
+    ]
+  };
+  tpl.files.unshift('./templates/tsql/db.swig');
+  tpl.db = swig.compileFile('./templates/tsql/db.swig');
+  return tpl;
 }
 /**
  * Creates a new random salt value.
@@ -122,7 +148,9 @@ function makeSalt() {
   var hrtime = process.hrtime(),
       value = hrtime[0] + process.pid + hrtime[1] + parseInt(Math.random(), 10),
       salt = 'D38298C1a5354BFA859f1FD2E0Fd3774',
-      hashids = new Hashids(salt, 12),
-      salt = hashids.encode(value);
+      hashids = new Hashids(salt, 12);
+  
+  salt = hashids.encode(value);
+  
   return salt;
 }
